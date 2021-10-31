7e95818e278da96e4a7cf87b36a408f83a55af30
rsohlot <5600987+rsohlot@users.noreply.github.com>
2021-10-31 13:26:11 +0530

    merge functions

--- AI.py
+++ AI.py
@@ -11,29 +11,6 @@ class Player(object):
         super(Player, self).__init__()
         self.score = 0
 
-    def get_reward(self,current_player, piece, step, board):
-        """
-        Get the reward of a piece.
-        """
-        reward = 0
-
-        peice_updated_pos = {} 
-        peice_reward = {}
-        for k,v in  board.piecesPosition.items():
-          if k[0] == current_player and k[1] == piece:
-            peice_updated_pos[k[1]] = v + step
-            # occupied_status = board.isOccupied( board.PATHS[0][v + step-1])
-            occupied_status = board.isOccupied(v + step-1)
-            if occupied_status[0] and occupied_status[1] != k[0]:
-              peice_reward[k[1]] = 40
-            else:
-              peice_reward[k[1]] = 10
-            return reward
-        ###
-        # peice_reward = sorted(peice_reward.items(), key=lambda x: x[1])
-        # reward = peice_reward[0][0]
-        # return reward
-
 
     def greedy_selection(self,current_player, pieces, step, board):
         """
@@ -41,17 +18,22 @@ class Player(object):
         """
         piece_with_max_reward = pieces[0]
         max_reward = 0
-        temp_score = 0
-        for each_piece in pieces:
-          reward = self.get_reward(current_player, each_piece, step, board)
+        reward = 0
+        for piece,each_piece_pos in  board.piecesPosition.items():
+          if piece[0] == current_player:
+            occupied_status = board.isOccupied(each_piece_pos + step-1)
+            if occupied_status[0] and occupied_status[1] != piece[0]:
+              reward = 40
+            else:
+              reward =10
+
           if reward > max_reward:
-              piece_with_max_reward = each_piece
+              piece_with_max_reward = piece[1]
               max_reward = reward
 
-        self.score += temp_score
+        self.score += max_reward
         return piece_with_max_reward
 
 
     def choose(self, current_player, pieces, step, board):
-        # return random.choice(pieces, step, board)
         return self.greedy_selection(current_player, pieces, step, board)
